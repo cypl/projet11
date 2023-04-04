38cd94b05c29ac334239fc3414811ac64e6c0fa1
cypl <cyrille.plenchette@me.com>
2023-04-04 22:17:53 +0200

    -

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@
 
 function App() {
   
-  fetch('./data/logements.json').then(
-    r => r.json()
-  ).then(
-    d => console.log(d)
-  )
-
-  // requestLodging().then(
-  //   lodging => {console.log(lodging)}
+  // fetch('./data/logements.json').then(
+  //   r => r.json()
+  // ).then(
+  //   d => console.log(d)
   // )
+
+  // fetch('./data/logements.json').then(
+  //   r => r.json()
+  // ).then(
+  //   d => {
+  //     const dataFetched = d; // assigner la valeur de d à la variable dataFetched
+  //     console.log(dataFetched); // afficher la valeur de dataFetched
+  // })
+
+  const dataFetched = fetch('./data/logements.json').then(
+  r => r.json()
+  ).then(
+  d => {
+  return d
+  })
+
+  console.log(
+    dataFetched.then(d => {return d})
+    );
+  
 
   return (
     <div className="App">
