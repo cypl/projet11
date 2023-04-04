7de8e020bcbe4b8af6dbe8226f8e886faa6f353e
cypl <cyrille.plenchette@me.com>
2023-04-04 22:22:55 +0200

    prettier config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import {requestLodging} from "./api/fetchData.js";
 
 function App() {
-  
   // fetch('./data/logements.json').then(
   //   r => r.json()
   // ).then(
@@ -16,23 +15,23 @@ function App() {
   //     console.log(dataFetched); // afficher la valeur de dataFetched
   // })
 
-  const dataFetched = fetch('./data/logements.json').then(
-  r => r.json()
-  ).then(
-  d => {
-  return d
-  })
+  const dataFetched = fetch('./data/logements.json')
+    .then((r) => r.json())
+    .then((d) => {
+      return d
+    })
 
   console.log(
-    dataFetched.then(d => {return d})
-    );
-  
+    dataFetched.then((d) => {
+      return d
+    })
+  )
 
   return (
     <div className="App">
       <p>Kasa</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
