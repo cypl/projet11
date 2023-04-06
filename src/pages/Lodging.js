81a30f07c17da5518b58cc2642a6ff9e39d26061
cypl <cyrille.plenchette@me.com>
2023-04-06 16:32:43 +0200

    fix css responsive cards

--- src/pages/Lodging.js
+++ src/pages/Lodging.js
@@ -1,10 +1,9 @@
 import { useParams } from 'react-router-dom'
-// import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 function Lodging() {
-  const { id } = useParams()
+  const { id } = useParams() // = élément qui se trouve dans l'URL, après le : dans la Route
 
   return (
     <div>
