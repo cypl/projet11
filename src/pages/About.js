9942a17e71ecf8088ed8c6d8c34588b9eead60b3
cypl <cyrille.plenchette@me.com>
2023-04-07 14:21:40 +0200

    refacto Collapse component

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Banner from '../components/Banner'
-import Collapse from '../components/collapse/Collapse'
+import Collapse from '../components/Collapse'
 
 function About() {
   const [data, setData] = useState([])
