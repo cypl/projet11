71399e74f7aa481ed5d6c11ea812f362ee7816d9
cypl <cyrille.plenchette@me.com>
2023-04-07 14:14:57 +0200

    add Collapse component and About page datas JSON

---
+++ src/components/collapse/Collapse.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+function Collapse({ title, contentSingle, contentList }) {
+  let [open, setOpen] = useState(false)
+  function openCollapse() {
+    open ? setOpen(false) : setOpen(true)
+  }
+  const isContentSingle = contentSingle
+  const isContentList = contentList
+
+  return (
+    <article className={open ? 'collapse active' : 'collapse'}>
+      <div className="collapse_title" onClick={openCollapse}>
+        <p>{title}</p>
+      </div>
+      <div className="collapse_content">
+        {/* Dans le cas où on affiche un seul paragraphe */}
+        {isContentSingle ? <p>{contentSingle}</p> : ''}
+        {/* Dans le cas où on affiche une liste d'éléments */}
+        {isContentList ? (
+          <ul>
+            {contentList.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        ) : (
+          ''
+        )}
+      </div>
+    </article>
+  )
+}
+
+export default Collapse
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Banner from '../components/Banner'
+import Collapse from '../components/collapse/Collapse'
 
 function About() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('./data/about.json')
+      .then((r) => r.json())
+      .then((d) => {
+        setData(d)
+      })
+  }, [])
+
   return (
     <div>
       <Header />
       <Banner srcImage="/assets/banner_about.jpg" />
+      <section className="section_about">
+        <div className="content_width">
+          <div className="content_about">
+            {data.map((aboutItem) => (
+              <Collapse
+                key={aboutItem.id}
+                title={aboutItem.title}
+                contentSingle={aboutItem.content}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   )
