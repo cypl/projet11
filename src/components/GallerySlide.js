5f745926d9c8da27bdb06fff0482fab0b66fbed9
cypl <cyrille.plenchette@me.com>
2023-04-14 10:41:33 +0200

    refacto GallerySlide component

--- src/components/GallerySlide.js
+++ src/components/GallerySlide.js
@@ -1,7 +1,21 @@
 function GallerySlide({ slide }) {
+  /**
+   * Fonction qui permet de récupérer le nom du fichier dans l'URL
+   * @param {*} url
+   * @returns le nom du fichier image sans l'extension
+   */
+  function getFileName(url) {
+    const parts = url.slide.split('/')
+    const lastPart = parts[parts.length - 1]
+    const file = lastPart.split('.')
+    return file[0] // = le nom du fichier sans l'extension
+  }
+
+  const fileName = getFileName({ slide })
+
   return (
     <figure className="gallery_slide">
-      <img src={slide} alt={slide} />
+      <img src={slide} alt={fileName} />
     </figure>
   )
 }
