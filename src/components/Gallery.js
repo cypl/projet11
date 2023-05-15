90ff4a31ab6d95f52d956885dea34777e4ca4161
cypl <cyrille.plenchette@me.com>
2023-05-15 14:15:32 +0200

    fix Gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,31 +8,45 @@ function Gallery({ content }) {
   const [slideIndex, setSlideIndex] = useState(0) // la galerie commence sur la première slide du tableau
   const [images, setContent] = useState(content)
 
+  function nextSlideIndex(slideIndex) {
+    slideIndex !== images.length - 1
+      ? setSlideIndex((slideIndex) => slideIndex + 1)
+      : setSlideIndex(0)
+  }
+
+  function prevSlideIndex(slideIndex) {
+    slideIndex !== 0
+      ? setSlideIndex((slideIndex) => slideIndex - 1)
+      : setSlideIndex(images.length - 1)
+  }
+
+  useEffect(() => {
+    // ce hook sert uniquement à générer un nouveau rendu du composant
+    // lorsque le state slideIndex change.
+  }, [slideIndex])
+
+  // ce hook sert à forcer le retour de slideIndex à 0 lorsque l'on change de logement
+  // la gallerie repart alors sur la première image.
   useEffect(() => {
     setSlideIndex(0)
     setContent(content)
   }, [content])
-
-  function nextSlide() {
-    setSlideIndex((current) => {
-      return (current + 1) % images.length
-    })
-  }
-  function prevSlide() {
-    setSlideIndex((current) => {
-      return images.length - 1 + current
-    })
-  }
 
   return (
     <div className="gallery_wrapper">
       <div className="gallery">
         <GallerySlide slide={images[slideIndex]} />
         {images.length > 1 && (
-          <GalleryButton action={() => prevSlide()} target="prev" />
+          <GalleryButton
+            action={() => prevSlideIndex(slideIndex)}
+            target="prev"
+          />
         )}
         {images.length > 1 && (
-          <GalleryButton action={() => nextSlide()} target="next" />
+          <GalleryButton
+            action={() => nextSlideIndex(slideIndex)}
+            target="next"
+          />
         )}
         {images.length > 1 && (
           <GalleryCounter index={slideIndex + 1} total={images.length} />
--- src/components/GallerySlide.js
+++ src/components/GallerySlide.js
@@ -1,23 +1,23 @@
 import PropTypes from 'prop-types'
 
 function GallerySlide({ slide }) {
-  /**
-   * Fonction qui permet de récupérer le nom du fichier dans l'URL
-   * @param {*} url
-   * @returns le nom du fichier image sans l'extension
-   */
-  function getFileName(url) {
-    const parts = url.slide.split('/')
-    const lastPart = parts[parts.length - 1]
-    const file = lastPart.split('.')
-    return file[0] // = le nom du fichier sans l'extension
-  }
+  // /**
+  //  * Fonction qui permet de récupérer le nom du fichier dans l'URL
+  //  * @param {*} url
+  //  * @returns le nom du fichier image sans l'extension
+  //  */
+  // function getFileName(url) {
+  //   const parts = url.slide.split('/')
+  //   const lastPart = parts[parts.length - 1]
+  //   const file = lastPart.split('.')
+  //   return file[0] // = le nom du fichier sans l'extension
+  // }
 
-  const fileName = getFileName({ slide })
+  // const fileName = getFileName({ slide })
 
   return (
     <figure className="gallery_slide">
-      <img src={slide} alt={fileName} />
+      <img src={slide} alt={slide} />
     </figure>
   )
 }
